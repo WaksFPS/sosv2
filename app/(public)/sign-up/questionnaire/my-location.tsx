863c0fc545d10ee4ba0196c4121b69f49c957292
Jeane Claude Maglaqui <jeane.maglaqui@hoolisoftware.com>
2024-08-07 18:26:01 +0800

    upload 2

--- app/(public)/sign-up/questionnaire/my-location.tsx
+++ app/(public)/sign-up/questionnaire/my-location.tsx
@@ -1,47 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { Alert, Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { BackButton } from '@/components/shared/back-button';
-import { Button } from '@/components/shared/button';
-import { Input } from '@/components/shared/input';
-import TextScalable from '@/components/shared/text-scale';
-import { updateUserFieldApi } from '@/services/endpoints/user';
-import { MyLocationType, myLocationSchema } from '@/services/endpoints/user/schema';
-import { authAtom } from '@/store/auth';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useMutation } from '@tanstack/react-query';
-import { router, Stack } from 'expo-router';
-import { useAtomValue } from 'jotai';
-import { useCountries, useStates, useCities, useLocationByPostalCode } from '@/services/external/useLocationData';
-import { Picker } from '@react-native-picker/picker';
+import { BackButton } from '@/components/shared/back-button'
+import { Button } from '@/components/shared/button'
+import { Input } from '@/components/shared/input'
+import TextScalable from '@/components/shared/text-scale'
+import { updateUserFieldApi } from '@/services/endpoints/user'
+import {
+    MyLocationType,
+    myLocationSchema,
+} from '@/services/endpoints/user/schema'
+import { authAtom } from '@/store/auth'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { router, Stack } from 'expo-router'
+import { useAtomValue } from 'jotai'
+import React from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import {
+    Alert,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    TouchableWithoutFeedback,
+    View,
+} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const MyLocation = () => {
-    const auth = useAtomValue(authAtom);
-    const [selectedCountry, setSelectedCountry] = useState<string>('');
-    const [selectedState, setSelectedState] = useState<string>('');
-    const [postalCode, setPostalCode] = useState<string>('');
+    const auth = useAtomValue(authAtom)
 
-    const { data: countries, isLoading: isLoadingCountries } = useCountries();
-    const { data: states, isLoading: isLoadingStates } = useStates(selectedCountry);
-    const { data: cities, isLoading: isLoadingCities } = useCities(selectedState);
-    const { data: locationData } = useLocationByPostalCode(postalCode);
+    const handleNext = (data: MyLocationType) => {
+        updateLocationMu.mutate(data)
+    }
 
     const myLocationForm = useForm<MyLocationType>({
         mode: 'all',
         resolver: zodResolver(myLocationSchema),
-    });
-
-    useEffect(() => {
-        if (locationData) {
-            myLocationForm.setValue('state', locationData.adminName1);
-            myLocationForm.setValue('city', locationData.placeName);
-        }
-    }, [locationData, myLocationForm]);
-
-    const handleNext = (data: MyLocationType) => {
-        updateLocationMu.mutate(data);
-    };
+    })
 
     const updateLocationMu = useMutation({
         mutationFn: (data: MyLocationType) =>
@@ -59,18 +52,12 @@ const MyLocation = () => {
                 params: {
                     ...myLocationForm.getValues(),
                 },
-            });
+            })
         },
         onError: () => {
-            Alert.alert('Error', 'An error occurred');
+            Alert.alert('Error', 'An error occurred')
         },
-    });
-
-    useEffect(() => {
-        console.log('Countries:', countries);
-        console.log('States:', states);
-        console.log('Cities:', cities);
-    }, [countries, states, cities]);
+    })
 
     return (
         <KeyboardAvoidingView
@@ -96,79 +83,75 @@ const MyLocation = () => {
                     />
                     <View className="flex-1 w-full items-center justify-center">
                         <View>
-                            <TextScalable fontSize={25} fontClassname="font-bold text-center text-sos-gray pt-10">
+                            <TextScalable
+                                fontSize={25}
+                                fontClassname="font-bold text-center text-sos-gray pt-10"
+                            >
                                 My Location
                             </TextScalable>
-                            <TextScalable fontSize={17} fontClassname="text-center text-sos-gray font-medium pb-10">
+                            <TextScalable
+                                fontSize={17}
+                                fontClassname="text-center text-sos-gray font-medium pb-10"
+                            >
                                 (Used to find people nearby)
                             </TextScalable>
                         </View>
+
                         <View className="w-full">
                             <Controller
                                 control={myLocationForm.control}
                                 name="country"
-                                render={({ field }) => (
-                                    <Picker
-                                        selectedValue={field.value}
-                                        onValueChange={(value) => {
-                                            field.onChange(value);
-                                            setSelectedCountry(value);
-                                        }}
-                                    >
-                                        <Picker.Item label="Select Country" value="" />
-                                        {countries?.map((country) => (
-                                            <Picker.Item key={country.geonameId} label={country.countryName} value={country.countryCode} />
-                                        ))}
-                                    </Picker>
+                                render={({ field, fieldState }) => (
+                                    <Input
+                                        containerClassName="my-1"
+                                        placeholder="Country"
+                                        value={field.value}
+                                        onChangeText={field.onChange}
+                                        onBlur={field.onBlur}
+                                        errorMessage={fieldState.error?.message}
+                                    />
                                 )}
                             />
                             <Controller
                                 control={myLocationForm.control}
                                 name="zip_code"
-                                render={({ field }) => (
+                                render={({ field, fieldState }) => (
                                     <Input
                                         containerClassName="my-1"
                                         placeholder="Postal Code"
                                         value={field.value}
-                                        onChangeText={(value) => {
-                                            field.onChange(value);
-                                            setPostalCode(value);
-                                        }}
+                                        onChangeText={field.onChange}
                                         onBlur={field.onBlur}
+                                        errorMessage={fieldState.error?.message}
                                     />
                                 )}
                             />
                             <Controller
                                 control={myLocationForm.control}
                                 name="state"
-                                render={({ field }) => (
-                                    <Picker
-                                        selectedValue={field.value}
-                                        onValueChange={(value) => {
-                                            field.onChange(value);
-                                            setSelectedState(value);
-                                        }}
-                                    >
-                                        <Picker.Item label="Select State" value="" />
-                                        {states?.map((state) => (
-                                            <Picker.Item key={state.geonameId} label={state.name} value={state.adminCode1} />
-                                        ))}
-                                    </Picker>
+                                render={({ field, fieldState }) => (
+                                    <Input
+                                        containerClassName="my-1"
+                                        placeholder="State"
+                                        value={field.value}
+                                        onChangeText={field.onChange}
+                                        onBlur={field.onBlur}
+                                        errorMessage={fieldState.error?.message}
+                                    />
                                 )}
                             />
                             <Controller
                                 control={myLocationForm.control}
                                 name="city"
-                                render={({ field }) => (
-                                    <Picker
-                                        selectedValue={field.value}
-                                        onValueChange={field.onChange}
-                                    >
-                                        <Picker.Item label="Select City" value="" />
-                                        {cities?.map((city) => (
-                                            <Picker.Item key={city.geonameId} label={city.name} value={city.geonameId} />
-                                        ))}
-                                    </Picker>
+                                render={({ field, fieldState }) => (
+                                    <Input
+                                        containerClassName="my-1"
+                                        placeholder="City"
+                                        value={field.value}
+                                        onChangeText={field.onChange}
+                                        onBlur={field.onBlur}
+                                        errorMessage={fieldState.error?.message}
+                                    />
                                 )}
                             />
                         </View>
@@ -189,7 +172,7 @@ const MyLocation = () => {
                 </SafeAreaView>
             </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
-    );
-};
+    )
+}
 
-export default MyLocation;
+export default MyLocation
